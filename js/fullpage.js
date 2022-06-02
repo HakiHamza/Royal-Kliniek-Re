614f34ab23de02e6d232f795e307e62366bcd03f
HakiHamza <haktan.boga@hotmail.com>
2022-06-02 11:43:56 +0200

    pillars plus links to slides

--- js/fullpage.js
+++ js/fullpage.js
@@ -1,4 +1,8 @@
 new fullpage("#fullpage", {
   autoScrolling: true,
   navigation: true,
+  anchors: ["s1", "s2", "s3", "s4"],
+  scrollingSpeed: 1200,
+  controlArrows: false,
+  slidesNavigation: true,
 });
