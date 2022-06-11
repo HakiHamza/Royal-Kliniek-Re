b117190de5dd9c2397d17e818d997dc2016e7399
HakiHamza <haktan.boga@hotmail.com>
2022-06-11 20:54:50 +0200

    team and navigation

--- js/fullpage.js
+++ js/fullpage.js
@@ -1,6 +1,9 @@
 new fullpage("#fullpage", {
   navigation: true,
   anchors: ["s1", "s2", "s3", "s4", "s5"],
+  navigationTooltips: ["Home", "Work", "Team", "Locatie", "Contact"],
+  showActiveTooltip: true,
   scrollingSpeed: 1200,
   slidesNavigation: true,
+  controlArrows: false,
 });
--- js/script.js
+++ js/script.js
@@ -1,16 +1,26 @@
 // NAVIGATION DROPDOWN
 
-const el = document.getElementById("icon-hover__dropdown");
-const hiddenDropdown = document.getElementById("dropdown-icon");
+// const el = document.getElementById("icon-hover__dropdown");
+// const hiddenDropdown = document.getElementById("dropdown-icon");
 
-el.addEventListener("mouseover", function handleMouseOver() {
-  hiddenDropdown.style.visibility = "visible";
-  hiddenDropdown.style.opacity = "1";
-});
+// el.addEventListener("mouseover", function handleMouseOver() {
+//   hiddenDropdown.style.visibility = "visible";
+//   hiddenDropdown.style.opacity = "1";
+// });
 
-el.addEventListener("mouseout", function handleMouseOut() {
-  hiddenDropdown.style.visibility = "hidden";
-  hiddenDropdown.style.opacity = "0";
+// el.addEventListener("mouseout", function handleMouseOut() {
+//   hiddenDropdown.style.visibility = "hidden";
+//   hiddenDropdown.style.opacity = "0";
+// });
+
+// TOGLE MENU
+
+const toggle = document.querySelector(".toggle");
+const navigation = document.querySelector(".navigation");
+
+toggle.addEventListener("click", () => {
+  toggle.classList.toggle("active");
+  navigation.classList.toggle("active");
 });
 
 // SMALL CONTACT VISIBILITY
@@ -19,11 +29,11 @@ el.addEventListener("mouseout", function handleMouseOut() {
 //   document.getElementById("contact").style.visibility = "visible";
 // }
 
-function smallContact() {
-  var hiddenContact = document.getElementById("contact");
-  if (hiddenContact.style.visibility === "hidden") {
-    hiddenContact.style.visibility = "visible";
-  } else {
-    hiddenContact.style.visibility = "hidden";
-  }
-}
+// function smallContact() {
+//   var hiddenContact = document.getElementById("contact");
+//   if (hiddenContact.style.visibility === "hidden") {
+//     hiddenContact.style.visibility = "visible";
+//   } else {
+//     hiddenContact.style.visibility = "hidden";
+//   }
+// }
