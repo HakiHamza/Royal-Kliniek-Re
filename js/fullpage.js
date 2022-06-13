4ccc88bc42d05952d9a17330e30576207ffd3687
HakiHamza <haktan.boga@hotmail.com>
2022-06-13 17:48:37 +0200

    button and socials links full styling

--- js/fullpage.js
+++ js/fullpage.js
@@ -3,7 +3,7 @@ new fullpage("#fullpage", {
   anchors: ["s1", "s2", "s3", "s4", "s5"],
   navigationTooltips: ["Home", "Work", "Team", "Locatie", "Contact"],
   showActiveTooltip: true,
-  scrollingSpeed: 1200,
+  scrollingSpeed: 1000,
   slidesNavigation: true,
   controlArrows: false,
 });
