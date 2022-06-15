6e41f72fbe8ba841d4dc018f09221e3132568eb7
HakiHamza <haktan.boga@hotmail.com>
2022-06-15 19:09:30 +0200

    custom arrows in sliders

--- js/fullpage.js
+++ js/fullpage.js
@@ -1,10 +1,13 @@
 new fullpage("#fullpage", {
-  navigation: true,
+  navigation: false,
   anchors: ["s1", "s2", "s3", "s4", "s5"],
   navigationTooltips: ["Home", "Work", "Team", "Locatie", "Contact"],
   showActiveTooltip: true,
   scrollingSpeed: 1000,
   slidesNavigation: true,
-  controlArrows: false,
-  responsiveWidth: 900,
+  controlArrows: true,
+  controlArrowsHTML: [
+    ' <div class=".fp-controlArrow"><div class="fp-prev"></div></div>',
+    '<div class=".fp-controlArrow"><div class="fp-next"></div></div>',
+  ],
 });
